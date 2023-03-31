9ca7dd69ea1db74372db953d3e4383a0137da423
MarcoSacch <MarcoSacch@users.noreply.github.com>
2023-03-31 17:22:02 -0300

    31 de marzo
    angular

---
+++ marco-proyecto-abril-nuevo/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BodyComponent } from './components/body/body.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { SobremiComponent } from './components/sobremi/sobremi.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { PortafolioComponent } from './components/portafolio/portafolio.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BodyComponent,
+    InicioComponent,
+    SobremiComponent,
+    ServiciosComponent,
+    HabilidadesComponent,
+    PortafolioComponent,
+    ContactoComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
+
+
+
+
